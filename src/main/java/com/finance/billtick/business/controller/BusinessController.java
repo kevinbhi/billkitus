aa35001dd458ac32ac9eb4935c1023af1ffd0196
kevinbhi <kevinkumarbhingaradiya@gmail.com>
2026-08-14 11:16:37 -0500

    Business: Update version

--- src/main/java/com/finance/billtick/business/controller/BusinessController.java
+++ src/main/java/com/finance/billtick/business/controller/BusinessController.java
@@ -1,6 +1,5 @@
 package com.finance.billtick.business.controller;
 
-
 import com.finance.billtick.business.model.Business;
 import com.finance.billtick.business.repository.BusinessRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +14,19 @@ public class BusinessController {
 
     private final BusinessRepository businessRepository;
 
-
     @PostMapping("/")
-    public Business createBusiness(@RequestBody Business businessRequest) {
-        return businessRepository.save(businessRequest);
+    public Business createBusiness(@RequestBody Business businessrequest) {
+        Business business = new Business();
+        business.setBusinessName(businessrequest.getBusinessName());
+        business.setCity(businessrequest.getCity());
+        business.setState(businessrequest.getState());
+        business.setZipCode(businessrequest.getZipCode());
+        business.setInvoicePrefix(businessrequest.getInvoicePrefix());
+        business.setDefaultTerms(businessrequest.getDefaultTerms());
+        business.setSalesTaxRate(businessrequest.getSalesTaxRate());
+        business.setLogo(businessrequest.getLogo());
+        businessRepository.save(business);
+        return business;
     }
 
     @GetMapping(value = "/")
@@ -26,19 +34,30 @@ public class BusinessController {
         return businessRepository.findAll();
     }
 
-    @GetMapping(value = "/{id}")
-    public Business getBusinessById(@PathVariable Long id) {
-        return businessRepository.findById(id).orElse(null);
+    @GetMapping(value = "/{Id}")
+    public Business getBusinessById(@PathVariable Integer Id) {
+        return businessRepository.findById(Long.valueOf(Id))
+                .orElseThrow(() -> new RuntimeException("Business not found"));
     }
 
-    @PutMapping(value = "/{id}")
-    public Business updateBusiness(@PathVariable Long id, @RequestBody Business businessRequest) {
-        return businessRepository.save(businessRequest);
+    @PutMapping(value = "/{Id}")
+    public Business updateBusiness(@PathVariable Integer Id, @RequestBody Business businessrequest) {
+        Business business = businessRepository.findById(Long.valueOf(Id))
+                .orElseThrow(() -> new RuntimeException("Business not found"));
+        business.setBusinessName(businessrequest.getBusinessName());
+        business.setCity(businessrequest.getCity());
+        business.setState(businessrequest.getState());
+        business.setZipCode(businessrequest.getZipCode());
+        business.setInvoicePrefix(businessrequest.getInvoicePrefix());
+        business.setDefaultTerms(businessrequest.getDefaultTerms());
+        business.setSalesTaxRate(businessrequest.getSalesTaxRate());
+        business.setLogo(businessrequest.getLogo());
+        businessRepository.save(business);
+        return business;
     }
 
-    @DeleteMapping(value = "/")
-    public void deleteBusiness(@RequestParam Long id) {
-        businessRepository.deleteById(id);
+    @DeleteMapping(value = "/{Id}")
+    public void deleteBusiness(@PathVariable Integer Id) {
+        businessRepository.deleteById(Long.valueOf(Id));
     }
-
 }
